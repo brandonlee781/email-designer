92179b0a34040030c50ea2d99bb98ded4302e2f7
Brandon Lee <brandonlee781@gmail.com>
2018-11-25 16:24:08 -0600

    Prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  hmr: false
+  hmr: false,
+  firebase: {
+    apiKey: 'AIzaSyBsd1HNt7mRwS2JB2_bRSCoVKseBt5jNWU',
+    authDomain: 'email-designer-5aa67.firebaseapp.com',
+    databaseURL: 'https://email-designer-5aa67.firebaseio.com',
+    projectId: 'email-designer-5aa67',
+    storageBucket: 'email-designer-5aa67.appspot.com',
+    messagingSenderId: '778482168820'
+  },
+  inlinerUrl: 'https://us-central1-email-designer-5aa67.cloudfunctions.net/htmlInliner',
 };
