d8e13daffcfcb20620675097c099d5c2062a54bc
Brandon Lee <brandonlee781@gmail.com>
2018-11-25 16:22:47 -0600

    Attempt to improve minified html modal rendering

--- src/app/features/email/components/email-dialog/email-dialog.component.ts
+++ src/app/features/email/components/email-dialog/email-dialog.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { DialogType } from '../../state/email';
-import { GenerateHtmlService } from '../../services/generate-html.service';
+import { GenerateHtmlService, ServiceResponse } from '../../services/generate-html.service';
+import { Observable, Subscription } from 'rxjs';
 
 export interface DialogData {
   type: DialogType;
@@ -12,40 +13,47 @@ export interface DialogData {
   templateUrl: './email-dialog.component.html',
   styleUrls: ['./email-dialog.component.scss']
 })
-export class EmailDialogComponent implements OnInit {
+export class EmailDialogComponent implements OnInit, OnDestroy {
   header: string;
   content: string;
   html: string;
   text: string;
+  showTextArea = false;
   copySuccess = false;
   copyError = false;
+
+  htmlServiceObservable: Subscription;
 
   constructor(
     public dialogRef: MatDialogRef<EmailDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
     private generateHtmlService: GenerateHtmlService,
-  ) {
-    this.generateHtmlService
+  ) {}
+
+  ngOnInit() {
+    this.htmlServiceObservable = this.generateHtmlService
       .html$
       .subscribe(({ html, text }) => {
         this.html = html;
         this.text = text;
+        switch (this.data.type) {
+          case 'generateHtml':
+            this.header = 'Generated Email HTML';
+            this.content = this.html;
+            break;
+          case 'generateText':
+            this.header = 'Generated Email Text';
+            this.content = this.text;
+            break;
+          default:
+            this.header = 'What did you do?';
+            break;
+        }
+        this.showTextArea = true;
       });
   }
 
-  ngOnInit() {
-    switch (this.data.type) {
-      case 'generateHtml':
-        this.header = 'Generated Email HTML';
-        this.content = this.html;
-        break;
-      case 'generateText':
-        this.header = 'Generated Email Text';
-        this.content = this.text;
-        break;
-      default:
-        this.header = 'What did you do?';
-        break;
-    }
+  ngOnDestroy() {
+    this.htmlServiceObservable.unsubscribe();
   }
 }
